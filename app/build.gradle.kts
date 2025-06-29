2887ce45e3397a05cccb06386e34ff57746bd815
Shastkiv Ruslan <162023673+ShastkivRuslan@users.noreply.github.com>
2025-06-29 20:18:39 +0300

    updated Build_gradle scripts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,19 +3,24 @@ import java.io.FileInputStream
 
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 val localProperties = Properties()
 val localPropertiesFile = rootProject.file("local.properties")
 if (localPropertiesFile.exists()) {
-    localProperties.load(FileInputStream(localPropertiesFile))
+    FileInputStream(localPropertiesFile).use { fis ->
+        localProperties.load(fis)
+    }
 }
+val apiKey: String = localProperties.getProperty("openai.api.key", "")
+val apiSystemPrompt: String = localProperties.getProperty("openai.system.promt", "")
 
-val apiKey = localProperties.getProperty("openai.api.key") ?: ""
-val apiSystemPromt = localProperties.getProperty("openai.system.promt") ?: ""
+
 
 android {
-
     namespace = "com.example.learnwordstrainer"
     compileSdk = 34
 
@@ -31,7 +36,7 @@ android {
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -47,25 +52,34 @@ android {
         viewBinding = true
         buildConfig = true
     }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.room)
-    annotationProcessor(libs.room.compiler)
-    implementation(libs.material)
-    implementation (libs.retrofit)
-    implementation (libs.converter.gson)
-    implementation (libs.squareup.logging.interceptor)
-    implementation (libs.android.lottie)
+    implementation(libs.core.ktx.v1131)
+    ksp(libs.room.compiler)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.squareup.logging.interceptor)
+    implementation(libs.android.lottie)
 
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 
+    implementation(libs.datastore.preferences)
+
+    implementation(libs.lifecycle.viewmodel.ktx)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    testImplementation(libs.kotlinx.coroutines.test)
 }
